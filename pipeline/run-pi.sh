30e7e4b254a4847ad1083e6bad9d6fe9ce6dacc7
C-D-Lewis <Bonsitm@gmail.com>
2021-01-14 22:01:43 +0000

    Remove redundant exports

--- pipeline/run-pi.sh
+++ pipeline/run-pi.sh
@@ -18,9 +18,6 @@ rm -rf chunky-fargate
   && cp ChunkyLauncher.jar chunky-fargate/
   && cd chunky-fargate
   && docker build -t chunky-fargate .
-  && export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-  && export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-  && export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
   && docker run
     -e \"WORLD_NAME=$WORLD_NAME\"
     -e \"SCENE_NAME=$SCENE_NAME\"
